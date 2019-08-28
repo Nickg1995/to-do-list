8768f6006843ae6692129994eb8fef51eab27e19
Nickg1995 <nickg1950@hotmail.com>
2019-08-28 11:07:41 +1000

    Updates

---
+++ precache-manifest.5ed3844d9f84b5046f05c7a9bb4a839d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28445f5553f514dc45a4256e8cfe6a9",
+    "url": "/Nickg1995/to-do-list/index.html"
+  },
+  {
+    "revision": "ca913e2a41d12be34e5d",
+    "url": "/Nickg1995/to-do-list/static/css/main.f2df2c9d.chunk.css"
+  },
+  {
+    "revision": "e7d08b8628222f2a4d93",
+    "url": "/Nickg1995/to-do-list/static/js/2.0cff5fc7.chunk.js"
+  },
+  {
+    "revision": "ca913e2a41d12be34e5d",
+    "url": "/Nickg1995/to-do-list/static/js/main.580ab0f7.chunk.js"
+  },
+  {
+    "revision": "45afed1e6c6f8bdba9d7",
+    "url": "/Nickg1995/to-do-list/static/js/runtime~main.21f9c6c5.js"
+  }
+]);
